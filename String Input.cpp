6f5c163592f8ce3159678c430736003d15871237
Vaishnavi Verma <vaiatsunrise@gmail.com>
2025-08-13 20:20:43 +0530

    Add files via upload

---
+++ String Input.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+
+    cout << "Enter the valid name for s: ";
+    getline(cin, s); 
+
+    cout << s << endl;
+    return 0;
+}
